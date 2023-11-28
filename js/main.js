b012503a6a44830a0e41ee7083ac68ed9d9fda28
c-rahul.m <c-rahul.m@versa-networks.com>
2023-11-28 09:00:08 +0530

    code modified

--- js/main.js
+++ js/main.js
@@ -43,3 +43,80 @@
 
 })(jQuery);
 
+
+
+// slick slider code
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: false,
+    centerPadding: '60px',
+    arrows: false,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
+
+// Range slider code
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+
+function updateGradient(rangeValue) {
+    const percentage = (rangeValue - slider.min) / (slider.max - slider.min) * 100;
+    slider.style.backgroundImage = `linear-gradient(90deg, #000000 ${percentage}%, transparent ${percentage}%)`;
+}
+
+// Update gradient onload
+updateGradient(slider.value);
+
+// Update gradient oninput
+slider.addEventListener('input', (e) => {
+    output.innerHTML = e.target.value;
+    updateGradient(e.target.value);
+});
